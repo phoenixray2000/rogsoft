a1e1051f63a019041e244ef3155845d3ae7dcba7
phoenixray2000 <42480743+phoenixray2000@users.noreply.github.com>
2019-10-01 21:10:42 +0800

    Update config.json.js

--- cfddns/config.json.js
+++ cfddns/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"1.0",
-"md5":"33d4cc6d09e95dcd2e62334331ec48a2",
+"version":"1.1",
+"md5":"",
 "home_url":"Module_cfddns.asp",
 "title":"CloudFlare DDNS",
 "description":"CloudFlare DDNS",
 "changelog":"",
-"build_date":"2019-07-10_23:30:14"
+"build_date":"2019-10-01_21:10:14"
 }
